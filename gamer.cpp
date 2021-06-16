ee6df7b37f860afadbb45c332d37ae72387e7faa
gwit65 <85566725+gwit65@users.noreply.github.com>
2021-06-16 19:49:45 +0300

    Revert "графика"
    This reverts commit eb92a7b3f46369931e8450ee5a727cb289deb583.

--- gamer.cpp
+++ gamer.cpp
@@ -3,23 +3,17 @@
 #include "gamer.hpp"
 #include <math.h>
 using namespace std;
-
 float gamer_x = 400.f;
 float gamer_y = 400.f;
-float angle_sped = 6;
-float angle = 0;
+float angle_sped = 8;
 float gamer_sped = 0;
-const int gamer_max_sped = 6;
-const int gamer_min_sped = -6;
-const float pi = 3.14159265;
 
-sf::Sprite gamer;
-sf::Texture gamerTexture;
+float pi = 3.14159265;
+sf::RectangleShape gamer(sf::Vector2f(30.f, 30.f));
 
 void InitGamer() {
-  gamerTexture.loadFromFile("Images/triangle.png");
-  gamer.setTexture(gamerTexture);
   gamer.setPosition(gamer_x,gamer_y);
+  gamer.setFillColor(sf::Color::White);
   gamer.setOrigin(sf::Vector2f(15.f,15.f));
 }
 
@@ -36,19 +30,18 @@ void UpdateGamer() {
   }
 
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-     angle -= angle_sped;
-     if (angle < -360) {
-
-       angle = 0;
+     angle_sped -= 3;
+     if (angle_sped < -360) {
+         angle_sped = 0;
      }
-     gamer.setRotation(angle);
+     gamer.setRotation(angle_sped);
     }
   if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-     angle += angle_sped;
-     if (angle > 360) {
-         angle = 0;
+     angle_sped += 3;
+     if (angle_sped > 360) {
+         angle_sped = 0;
      }
-     gamer.setRotation(angle);
+     gamer.setRotation(angle_sped);
   }
 
   if (gamer_sped > 0) {
@@ -58,8 +51,9 @@ void UpdateGamer() {
       gamer_sped += 1;
   }
 
-  gamer_x = gamer_x + (gamer_sped * cos(angle*pi/180));
-  gamer_y = gamer_y + (gamer_sped * sin(angle*pi/180));
+  gamer_x = gamer_x + (gamer_sped * cos(angle_sped*pi/180));
+  gamer_y = gamer_y + (gamer_sped * sin(angle_sped*pi/180));
+
   gamer.setPosition(gamer_x,gamer_y);
   cout << angle_sped << endl;
 }
--- gamer.hpp
+++ gamer.hpp
@@ -1,6 +1,10 @@
 #ifndef GAMER_HPP_
 #define GAMER_HPP_
+
 #include <SFML/Graphics.hpp>
+
+const int gamer_max_sped = 6;
+const int gamer_min_sped = -6;
 
 void InitGamer();
 void UpdateGamer();
