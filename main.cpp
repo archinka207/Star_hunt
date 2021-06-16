ae87dfbb91b2cbda64e1ba43d753e845a7203f11
gwit65 <85566725+gwit65@users.noreply.github.com>
2021-06-16 19:43:30 +0300

    Revert "fix"
    This reverts commit 5d30e89a9ad38d4feef1a5c525131db914831a5b.

--- main.cpp
+++ main.cpp
@@ -2,31 +2,48 @@
 #include <iostream>
 #include "game.hpp"
 #include "gamestate.hpp"
-#include "gamer.hpp"
 
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
-  Game::ChangeState(new MenuState()); //press enter
-  Gamer::Init();
+
+<<<<<<< HEAD
+  ChangeGameState(new MenuState()); //press enter //дэбил на русском пиши
+
   sf::Time sleepTime = sf::milliseconds(40);
   while (window.isOpen()) {
     sf::Event event;
-
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
+=======
+    Game::ChangeState(new MenuState()); //press enter
+
+    sf::Time sleepTime = sf::milliseconds(40);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        Game::Update();
+
+        //-----
+        window.clear();
+        Game::Draw(window);
+        window.display();
+        sf::sleep(sleepTime);
+>>>>>>> f98ec2c90f1a5160acfa18305599fbb5636f6b05
     }
 
-    Game::Update();
-    Gamer::Update();
-
+    UpdateGame();
+    //-----
     window.clear();
-    Game::Draw(window);
-    Game::Draw(window);
+    DrawGame(window);
     window.display();
-
     sf::sleep(sleepTime);
   }
 }
